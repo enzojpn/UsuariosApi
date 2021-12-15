5e1a89dec716d439042f59a3a04cad63a67d1c41
bruno <enzo_jpn@hotmail.com>
2021-12-14 21:48:43 -0300

    adicionando modelo de Token

---
+++ Models/Token.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UsuarioApi.Models
+{
+    public class Token
+    {
+        public Token(string value)
+        {
+            Value = Value;
+        }
+        public string Value { get; }
+
+    }
+}
